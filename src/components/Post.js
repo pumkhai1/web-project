d7eb6d85474357ea38d44ff345f314bd16625c69
Pum Khai <pumk2000@gmail.com>
2022-08-10 00:52:20 -0400

    change them

--- src/components/Post.js
+++ src/components/Post.js
@@ -9,7 +9,6 @@ const Post = ({ post: { title, body, imgUrl, author }, index }) => {
   return (  
 
     <div>
-      <br />
       <div className="post-container">
         <h2 className="heading">{title}</h2>
         <img className="image" src={imgUrl} alt="post" />
