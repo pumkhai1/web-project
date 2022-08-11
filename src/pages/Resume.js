64674d9e051a99f5e658fea37ecd4b5e79795fd8
Pum Khai <pumk2000@gmail.com>
2022-08-10 20:37:14 -0400

    new changes

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,7 +3,14 @@ import resume from '../assets/resume.png';
 import '../styles/Resume.css';
 
 function Resume(){
-    return <embed src={resume} width="100%" height="100%"/>
+    return (
+        <div className='Resume' >
+            <br />
+            <br />
+            <br />
+            <embed src={resume} width="100%" height="100%"/>
+        </div>
+    )
 }
 
 export default Resume;
